afce08ae0069bc8cc7cced2b52297c044fa40e05
willsmartin <willsmartin@users.noreply.github.com>
2016-10-04 07:20:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -88,16 +88,10 @@ app.get('/', function (req, res) {
 });
 
 app.get('/:articleName', function (req, res) {
+    articleName = req.params.articleName;
     res.send(createTemplate(articles[articleName]));
 });
 
-app.get('/article-two', function (req, res) {
-    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-
-app.get('/article-three', function (req, res) {
-    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
