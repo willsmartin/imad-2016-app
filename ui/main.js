bf8372607fcd46ad127a64f90af00759a2b11da6
willsmartin <willsmartin@users.noreply.github.com>
2016-10-06 06:43:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,9 @@
 console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML = 'New value';
+
+//Move the image
+var element = document.getElementById('img');
+img.onclick = function() {
+    img.style.marginLeft = '100px';
+};
