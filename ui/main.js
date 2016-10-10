5f8581b94bf86e3bef294d2d937ce76cebde92e2
willsmartin <willsmartin@users.noreply.github.com>
2016-10-10 21:48:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -35,5 +35,5 @@ submit.onclick = function() {
         list += '<li>' + names[i] + '<li/>';
     }
     var ul = document.getElementById('namelist');
-    ui.innerHTML = list;
+    ul.innerHTML = list;
 };
