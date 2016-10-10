46761b9b7fd7a293a955ee15268d42e8470a27dc
willsmartin <willsmartin@users.noreply.github.com>
2016-10-10 21:44:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,4 +30,10 @@ submit.onclick = function() {
     //Make a request to the server and send the name
     //Capture a list of name and render it as a list
     var names = ['name1','name2','name3'];
+    var list = '';
+    for(var i=0; i < names.length; i++) {
+        list += '<li>' + names[i] + '<li/>';
+    }
+    var ul = document.getElementById('namelist');
+    ui.innerHTML = list;
 };
