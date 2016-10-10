dbf6154655b747b4ce13bcebcc65fdd0061f226b
willsmartin <willsmartin@users.noreply.github.com>
2016-10-10 07:04:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-counter = 0;
+var counter = 0;
 app.get('/counter', function (req, res) {
     counter = counter + 1;
     res.send(counter.toString());
