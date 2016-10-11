c0654f6b80aa10dc1bf02de2b1fe527af4a6e6d5
willsmartin <willsmartin@users.noreply.github.com>
2016-10-11 06:20:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -46,6 +46,6 @@ submit.onclick = function() {
     }
   };
   //Make the request
-  request.open('GET', 'http://willsmartin.imad.hasura-app.io/submit-name?name=' + , true);
+  request.open('GET', 'http://willsmartin.imad.hasura-app.io/submit-name?name=' + name, true);
   request.send(null);
 };
