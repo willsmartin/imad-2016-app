d5d9450e9e3b021c30afce9510197e82b597cc8d
willsmartin <willsmartin@users.noreply.github.com>
2016-10-11 06:18:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,13 +27,21 @@ var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
-    //Make a request to the server and send the name
-    //Capture a list of name and render it as a list
-    var names = ['name1','name2','name3','name4'];
-    var list = '';
-    for(var i=0; i < names.length; i++) {
-        list += '<li>' + names[i] + '</li>';
+     //Create a request object
+  var request = new XMLHttpRequest();
+  
+  //Capture the response and store it in a variable
+  request.onreadystatechange = function () {
+    if(request.readyState === XMLHttpRequest.DONE) {
+     if(request.status === 200)  {
+         //Capture a list of name and render it as a list
+        var names = ['name1','name2','name3','name4'];
+        var list = '';
+        for(var i=0; i < names.length; i++) {
+            list += '<li>' + names[i] + '</li>';
+        }
+        var ul = document.getElementById('namelist');
+        ul.innerHTML = list;
+     }
     }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
 };
